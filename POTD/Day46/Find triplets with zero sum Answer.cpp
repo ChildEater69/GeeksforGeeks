26cbaf456dc0eabc4f2a38838defe1f16b1fc7f2
Avdhesh Kumar Sharma <115105709+ChildEater69@users.noreply.github.com>
2023-07-08 10:40:59 +0530

    new

---
+++ POTD/Day46/Find triplets with zero sum Answer.cpp
@@ -0,0 +1,15 @@
+ bool findTriplets(int arr[], int n)
+    { 
+        //Your code here
+        sort(arr,arr+n);
+        for(int i=0;i<n-2;i++){
+            int j=i+1,k=n-1;
+            while(j<k){
+                int temp=arr[i]+arr[j]+arr[k];
+                if(temp==0)return true;
+                else if(temp<0)j++;
+                else k--;
+            }
+        }
+        return false;
+    }
